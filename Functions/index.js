fd0d07b17b7fda13fabb565c5532415c91dd9a97
SCEllis <40131246+Zergalotz@users.noreply.github.com>
2024-12-05 17:08:58 -0600

    added css, js, and html files with a greeting function

---
+++ Functions/index.js
@@ -0,0 +1,7 @@
+let greetingsString = "Hello World! This is JavaScript function in action."
+
+function getGreetings (greetingsString) {
+  return greetingsString;
+}
+
+console.log(getGreetings(greetingsString));
