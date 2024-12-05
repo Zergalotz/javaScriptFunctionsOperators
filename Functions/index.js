0294f94fc18a08577d86fb0ad28f69ee23a00edd
SCEllis <40131246+Zergalotz@users.noreply.github.com>
2024-12-05 17:10:53 -0600

    cleaning up main branch

--- Functions/index.js
+++
@@ -1,7 +0,0 @@
-let greetingsString = "Hello World! This is JavaScript function in action."
-
-function getGreetings (greetingsString) {
-  return greetingsString;
-}
-
-console.log(getGreetings(greetingsString));
